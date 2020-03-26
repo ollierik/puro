15923b14892e7783d367512bb94f6230bf37aa7f
ollierikkeskinen <keskinen.olli@gmail.com>
2020-03-26 17:57:39 +0200

    WIP Before refactoring MemoryManager interface

--- src/Pool.hpp
+++ src/Pool.hpp
@@ -1,8 +1,7 @@
-#include <array>
 #include <vector>
 #include <memory>
 
-
+/** Stores elements into stack, fixed size. */
 template <class ElementType, int PoolSize>
 class StackMemoryManager
 {
@@ -10,50 +9,23 @@ public:
     constexpr size_t size() { return PoolSize; };
     ElementType& operator[] (size_t i)
     {
-        //return (ElementType&)elements[i * sizeof(ElementType)];
         return reinterpret_cast<ElementType&>(elements[i * sizeof(ElementType)]);
     }
 
     ElementType* ptr(size_t i)
     {
-        //return (ElementType*)(&elements[i * sizeof(ElementType)]);
         return reinterpret_cast<ElementType*>(&elements[i * sizeof(ElementType)]);
     }
 private:
     void* elements[PoolSize * sizeof(ElementType)];
 };
-
-
-
-
-template <class ElementType, int PoolSize>
-class DynamicMemoryManager
-{
-public:
-    constexpr size_t size() { return PoolSize; };
-    ElementType& operator[] (size_t i)
-    {
-        //return (ElementType&)elements[i * sizeof(ElementType)];
-        return reinterpret_cast<ElementType&>(elements[i * sizeof(ElementType)]);
-    }
-
-    ElementType* ptr(size_t i)
-    {
-        //return (ElementType*)(&elements[i * sizeof(ElementType)]);
-        return reinterpret_cast<ElementType*>(&elements[i * sizeof(ElementType)]);
-    }
-private:
-    void* elements[PoolSize * sizeof(ElementType)];
-};
-
-
 
 
 
 /** Pool with fixed amount of elements. Allocates elements to stack.
 */
 template <class ElementType, int PoolSize>
-class Pool
+class StackPool
 {
 public:
 
@@ -61,7 +33,7 @@ public:
     {
     public:
 
-        Iterator(Pool& p, int i)
+        Iterator(StackPool& p, int i)
             : pool(p)
             , iteratorIndex(i)
         {}
@@ -96,18 +68,18 @@ public:
 
     private:
 
-        friend class Pool<ElementType, PoolSize>;
+        friend class StackPool<ElementType, PoolSize>;
 
         const size_t getLookupIndex() { return pool.lookup[iteratorIndex]; }
         ElementType* getElement() { return &pool.elements[getLookupIndex()]; }
 
         bool shouldContinue() const { return (iteratorIndex < pool.numInUse); }
 
-        Pool<ElementType, PoolSize>& pool;
+        StackPool<ElementType, PoolSize>& pool;
         int iteratorIndex;
     };
 
-    Pool()
+    StackPool()
     {
         for (int i=0; i<PoolSize; ++i)
         {
@@ -115,7 +87,7 @@ public:
         }
     }
 
-    Pool::Iterator begin() { return Pool::Iterator(*this, 0); }
+    StackPool::Iterator begin() { return StackPool::Iterator(*this, 0); }
 
     /** Returns int, used as a flag to check for depletion in the Iterator's ++ */
     int end() { return 0; } 
@@ -132,7 +104,7 @@ public:
         return nullptr;
     }
 
-    void remove(Pool::Iterator& it)
+    void remove(StackPool::Iterator& it)
     {
         std::cout << "\nREMOVE: " << it.getLookupIndex() << std::endl << std::endl;;
 
@@ -157,4 +129,132 @@ public:
     size_t numInUse = 0;
     StackMemoryManager<ElementType, PoolSize> elements;
     StackMemoryManager<size_t, PoolSize> lookup;
+};
+
+template <class ElementType, int InitialPoolSize = 32, int MaxPoolSize = -1>
+class DynamicPool
+{
+public:
+
+    using ElementPtr = std::unique_ptr<ElementType>;
+
+    class Iterator
+    {
+    public:
+
+        Iterator(StackPool& p, int i)
+            : pool(p)
+            , iteratorIndex(i)
+        {}
+
+        Iterator& operator*()
+        {
+            return *this;
+        }
+
+        ElementType* operator->()
+        {
+            return getElement();
+        }
+
+        bool operator!= (const int& /* dummy */)
+        {
+            return shouldContinue();
+        }
+
+        Iterator& operator++()
+        {
+            ++iteratorIndex;
+            return *this;
+        }
+        
+        void print()
+        {
+            std::cout << "Iterator: " << std::endl;
+            std::cout << "    iteratorIndex: " << iteratorIndex << std::endl;
+            std::cout << "    lookupIndex: " << getLookupIndex() << std::endl;
+        }
+
+    private:
+
+        //friend class StackPool<ElementType, PoolSize>;
+
+        const size_t getLookupIndex() { return pool.lookup[iteratorIndex]; }
+        ElementType* getElement() { return &pool.elements[getLookupIndex()]; }
+
+        bool shouldContinue() const { return (iteratorIndex < pool.numInUse); }
+
+        //StackPool<ElementType, PoolSize>& pool;
+        int iteratorIndex;
+    };
+
+
+    DynamicPool()
+    {
+        for (int i=0; i<PoolSize; ++i)
+        {
+            lookup[i] = i;
+        }
+    }
+
+    DynamicPool::Iterator begin() { return DynamicPool::Iterator(*this, 0); }
+
+    /** Returns int, used as a flag to check for depletion in the Iterator's ++ */
+    int end() { return 0; } 
+
+    template <class ...Args>
+    ElementType* add(Args... args)
+    {
+        // if we have already allocated elements that are not in use
+        if (numInUse < elements.size())
+        {
+        
+        }
+        // if we need to create new unique_ptrs
+        else if (elements.size() == elements.capacity())
+        {
+            if (! attemptToIncreaseSize())
+                return nullptr;
+
+        }
+
+        elements.emplace_back(std::forward(std::make_unique<ElementType>(args...));
+    }
+
+    void remove(DynamicPool::Iterator& it)
+    {
+        std::cout << "\nREMOVE: " << it.getLookupIndex() << std::endl << std::endl;;
+
+        const auto indexToRemove = it.getLookupIndex();
+        const auto indexOfLast = --numInUse;
+
+        if (indexOfLast > 0)
+        {
+            const auto temp = lookup[indexToRemove];
+            lookup[indexToRemove] = lookup[indexOfLast];
+            lookup[indexOfLast] = temp;
+        }
+
+        it.iteratorIndex -= 1;
+    }
+
+    size_t size() const
+    {
+        return elements.size();
+    }
+
+    bool tryToIncreaseCapacity()
+    {
+        const auto capacityBefore = elements.capacity();
+        int targetCapacity = elements.capacity() * 2;
+        if (MaxPoolSize < 0 && targetCapacity > MaxPoolSize)
+            targetCapacity = MaxPoolSize;
+
+        elements.reserve(targetCapacity);
+
+        return (elements.capacity > capacityBefore);
+    }
+
+    size_t numInUse = 0;
+    std::vector<ElementType> elements;
 };

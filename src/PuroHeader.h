818f668ac3e05503397a14c8fb6d37038a9207c8
ollierikkeskinen <keskinen.olli@gmail.com>
2020-03-31 22:28:45 +0300

    Unity build setup, moved includes to single file

---
+++ src/PuroHeader.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include <memory>
+#include <cstdlib>
+#include <intrin.h> // MSVS breakpoint
+#include <cmath>
+#include <utility>
+
+#include "definitions.hpp"
+#include "Parameter.hpp"
+#include "Envelope.hpp"
+#include "AudioSource.hpp"
+#include "Grain.hpp"
+#include "Pool.hpp"
+#include "Engine.hpp"
+#include "Controller.hpp"
